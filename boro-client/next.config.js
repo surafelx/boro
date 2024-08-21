dbacb2e97a3670f24c6b908989f343b6f0f8805f
Surafel Yimam <63095824+surafelx@users.noreply.github.com>
2024-08-21 10:43:27 +0300

    Initial

---
+++ boro-client/next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // experimental: {
+  //   // Required:
+  //   appDir: true,
+  // },
+  ...(process.env.NODE_ENV === 'production' && {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  }),
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+      },
+    ];
+  },
+};
